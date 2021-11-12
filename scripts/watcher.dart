bc39015b7b87a8ed5db15481fad901feaed2386f
gustavo-hillesheim <guhillesheim@gmail.com>
2021-11-12 11:01:27 -0300

    Improvements in watcher script

--- scripts/watcher.dart
+++ scripts/watcher.dart
@@ -1,31 +1,45 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:path/path.dart';
+import 'package:synchronized/synchronized.dart';
+
+final lock = Lock();
+Timer? debounceTimer;
+Process? process;
 
 void main(List<String> args) {
-  Process? process;
-  Future<void> runProcess() async {
-    if (process != null) {
-      print('[WATCHER] Killing already existing process...');
-      process!.kill();
-    }
-    print('[WATCHER] Running ${args.join(' ')}');
-    final whereResult = await Process.run('where', [args[0]]);
-    final commandPath = whereResult.stdout.toString().replaceAll('\n', '');
-    process = await Process.start(
-      commandPath,
-      args.sublist(1),
-      mode: ProcessStartMode.inheritStdio,
-      runInShell: true,
-    );
-  }
-
-  runProcess();
+  runProcess(args);
   Directory.current.watch(recursive: true).listen((e) {
     final currentDir = Directory.current.path;
     if (e.path.startsWith(join(currentDir, 'lib')) ||
         e.path.startsWith(join(currentDir, 'test'))) {
-      runProcess();
+      debounce(() => runProcess(args), const Duration(milliseconds: 500));
     }
+  });
+}
+
+void debounce(void Function() fn, Duration duration) {
+  if (debounceTimer != null && debounceTimer!.isActive) {
+    debounceTimer!.cancel();
+  }
+  debounceTimer = Timer(duration, () {
+    fn();
+  });
+}
+
+Future<void> runProcess(List<String> args) {
+  return lock.synchronized(() async {
+    if (process != null) {
+      print('[WATCHER] Killing already existing process...');
+      process!.kill();
+    }
+    print('[WATCHER] Running "${args.join(' ')}"...');
+    process = await Process.start(
+      args[0],
+      args.sublist(1),
+      mode: ProcessStartMode.inheritStdio,
+      runInShell: true,
+    );
   });
 }
