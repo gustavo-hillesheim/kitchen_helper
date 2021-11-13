307a989cb79d59c2c8acde91086e3dcd7127754e
Gustavo Hillesheim <guhillesheim@gmail.com>
2021-11-13 18:59:10 -0300

    Added GetIngredientUseCase

--- lib/core/repository.dart
+++ lib/core/repository.dart
@@ -5,4 +5,5 @@ import 'failure.dart';
 abstract class Repository<T, ID> {
   Future<Either<Failure, T>> create(T entity);
   Future<Either<Failure, T>> update(T entity);
+  Future<Either<Failure, T>> findById(ID id);
 }
---
+++ lib/domain/usecases/get_ingredient_usecase.dart
@@ -0,0 +1,17 @@
+import 'package:fpdart/fpdart.dart';
+
+import '../data/ingredient_repository.dart';
+import '../../core/failure.dart';
+import '../../core/usecase.dart';
+import '../models/ingredient.dart';
+
+class GetIngredientUseCase extends UseCase<String, Ingredient> {
+  final IngredientRepository repository;
+
+  GetIngredientUseCase(this.repository);
+
+  @override
+  Future<Either<Failure, Ingredient>> execute(String id) {
+    return repository.findById(id);
+  }
+}
