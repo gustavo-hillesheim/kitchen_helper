1f7c2f047a6fe975286da14fd7593a31246017ac
gustavo-hillesheim <guhillesheim@gmail.com>
2021-12-15 14:17:47 -0300

    Refactored SaveRecipeUseCase

--- lib/domain/usecases/recipe/save_recipe_usecase.dart
+++ lib/domain/usecases/recipe/save_recipe_usecase.dart
@@ -4,84 +4,14 @@ import '../../../core/core.dart';
 import '../../domain.dart';
 
 class SaveRecipeUseCase extends UseCase<Recipe, Recipe> {
-  static const allIngredientsMustExistMessage = "É necessário salvar os "
-      "ingredientes antes da receita";
+  final RecipeRepository repository;
 
-  final SQLiteDatabase database;
-  final IngredientRepository ingredientRepository;
-  final RecipeRepository recipeRepository;
-  final RecipeIngredientRepository recipeIngredientRepository;
-
-  SaveRecipeUseCase(
-    this.database,
-    this.ingredientRepository,
-    this.recipeRepository,
-    this.recipeIngredientRepository,
-  );
+  SaveRecipeUseCase(this.repository);
 
   @override
-  Future<Either<Failure, Recipe>> execute(Recipe recipe) async {
-    return _validateIngredients(recipe.ingredients).onRightThen(
-        (_) => database.insideTransaction(() => _saveRecipe(recipe)));
-  }
-
-  Future<Either<Failure, Recipe>> _saveRecipe(Recipe recipe) {
-    return recipeRepository
+  Future<Either<Failure, Recipe>> execute(Recipe recipe) {
+    return repository
         .save(recipe)
-        .onRightThen((id) => Right(recipe.copyWith(id: id)))
-        .onRightThen((recipe) async {
-      final saveIngredientsResult = await _saveIngredients(recipe);
-      if (saveIngredientsResult.isLeft()) {
-        return Left(saveIngredientsResult.getLeft().toNullable()!);
-      }
-      return Right(recipe);
-    });
-  }
-
-  Future<Either<Failure, void>> _validateIngredients(
-      List<RecipeIngredient> ingredients) async {
-    for (final ingredient in ingredients) {
-      final result = await _validateIngredient(ingredient);
-      if (result.isLeft()) {
-        return result;
-      }
-    }
-    return const Right(null);
-  }
-
-  Future<Either<Failure, void>> _validateIngredient(
-      RecipeIngredient ingredient) async {
-    final existsResult = ingredient.type == RecipeIngredientType.ingredient
-        ? await ingredientRepository.exists(ingredient.id)
-        : await recipeRepository.exists(ingredient.id);
-    if (existsResult.isLeft()) {
-      return existsResult;
-    }
-    final exists = existsResult.getRight().getOrElse(() => false);
-    if (!exists) {
-      return const Left(BusinessFailure(allIngredientsMustExistMessage));
-    }
-    return const Right(null);
-  }
-
-  Future<Either<Failure, void>> _saveIngredients(Recipe recipe) async {
-    for (final ingredient in recipe.ingredients) {
-      /*final idResult =
-          await recipeIngredientRepository.findId(recipe, ingredient);
-      if (idResult.isLeft()) {
-        return idResult;
-      }*/
-      final ingredientEntity = RecipeIngredientEntity.fromModels(
-        recipe,
-        ingredient,
-        id: 1,
-      );
-      final saveResult =
-          await recipeIngredientRepository.save(ingredientEntity);
-      if (saveResult.isLeft()) {
-        return saveResult;
-      }
-    }
-    return const Right(null);
+        .onRightThen((id) => Right(recipe.copyWith(id: id)));
   }
 }
--- test/domain/usecases/recipe/save_recipe_usecase_test.dart
+++ test/domain/usecases/recipe/save_recipe_usecase_test.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:kitchen_helper/core/core.dart';
@@ -10,116 +8,34 @@ import '../../../mocks.dart';
 
 void main() {
   late SaveRecipeUseCase usecase;
-  late IngredientRepository ingredientRepository;
-  late RecipeRepository recipeRepository;
-  late RecipeIngredientRepository recipeIngredientRepository;
-  late SQLiteDatabase database;
+  late RecipeRepository repository;
 
   setUp(() {
-    registerFallbackValue(FakeRecipeIngredientEntity());
-    registerFallbackValue(FakeRecipeIngredient());
     registerFallbackValue(FakeRecipe());
-    ingredientRepository = IngredientRepositoryMock();
-    recipeRepository = RecipeRepositoryMock();
-    recipeIngredientRepository = RecipeIngredientRepositoryMock();
-    database = SQLiteDatabaseMock();
-    usecase = SaveRecipeUseCase(
-      database,
-      ingredientRepository,
-      recipeRepository,
-      recipeIngredientRepository,
-    );
+    repository = RecipeRepositoryMock();
+    usecase = SaveRecipeUseCase(repository);
   });
 
-  void mockDatabaseTransaction() {
-    when(() => database.insideTransaction(any()))
-        .thenAnswer((invocation) async {
-      final future = invocation.positionalArguments[0]()
-          as FutureOr<Either<Failure, Recipe>>;
-      return future;
-    });
-  }
-
-  void mockRecipeIngredientRepositorySave(Either<Failure, int> result) {
-    when(() => recipeIngredientRepository.save(any()))
-        .thenAnswer((_) async => result);
-  }
-
-  void mockRecipeIngredientRepositoryFindId(Either<Failure, int?> result) {
-    when(() => recipeIngredientRepository.findId(any(), any()))
-        .thenAnswer((_) async => result);
-  }
-
   void mockRecipeRepositorySave(Either<Failure, int> result) {
-    when(() => recipeRepository.save(any())).thenAnswer((_) async => result);
+    when(() => repository.save(any())).thenAnswer((_) async => result);
   }
 
-  void mockIngredientRepositoryExists(Either<Failure, bool> result) {
-    when(() => ingredientRepository.exists(any()))
-        .thenAnswer((_) async => result);
-  }
-
-  void mockRecipeRepositoryExists(Either<Failure, bool> result) {
-    when(() => recipeRepository.exists(any())).thenAnswer((_) async => result);
-  }
-
-  test(
-    'WHEN not all ingredients from the recipe are saved SHOULD return Failure',
-    () async {
-      mockRecipeIngredientRepositoryFindId(const Right(1));
-      mockRecipeIngredientRepositorySave(const Right(1));
-      mockRecipeRepositorySave(const Right(1));
-      mockIngredientRepositoryExists(const Right(false));
-
-      final result = await usecase.execute(sugarWithEggRecipeWithId);
-
-      expect(result.isLeft(), true);
-      expect(result.getLeft().toNullable()!.message,
-          SaveRecipeUseCase.allIngredientsMustExistMessage);
-
-      verify(() => ingredientRepository.exists(any()));
-    },
-  );
-
-  test(
-      'WHEN recipe have ingredients of type ingredient and recipe '
-      'SHOULD check both repositories', () async {
-    mockIngredientRepositoryExists(const Right(true));
-    mockRecipeRepositoryExists(const Right(true));
-    mockDatabaseTransaction();
-    mockRecipeRepositorySave(const Right(1));
-    mockRecipeIngredientRepositoryFindId(const Right(1));
-    mockRecipeIngredientRepositorySave(const Right(1));
+  test('WHEN called SHOULD save the recipe', () async {
+    mockRecipeRepositorySave(const Right(2));
 
     final result = await usecase.execute(cakeRecipe);
 
-    expect(result.isRight(), true);
-    verify(() => ingredientRepository.exists(any()));
-    verify(() => recipeRepository.exists(any()));
-    verify(() => database.insideTransaction(any()));
+    expect(result.getRight().toNullable(), cakeRecipe.copyWith(id: 2));
+    verify(() => repository.save(cakeRecipe));
   });
 
-  test('WHEN called with a valid recipe SHOULD save it', () async {
-    mockIngredientRepositoryExists(const Right(true));
-    mockDatabaseTransaction();
-    mockRecipeRepositorySave(const Right(1));
-    mockRecipeIngredientRepositoryFindId(const Right(null));
-    mockRecipeIngredientRepositorySave(const Right(1));
+  test('WHEN repository returns Failure SHOULD return Failure too', () async {
+    final failure = FakeFailure('repository failure');
+    mockRecipeRepositorySave(Left(failure));
 
-    final result = await usecase.execute(sugarWithEggRecipeWithoutId);
+    final result = await usecase.execute(cakeRecipe);
 
-    expect(result.isRight(), true);
-    final savedRecipe = result.getRight().toNullable()!;
-    expect(savedRecipe, sugarWithEggRecipeWithId);
-    verify(() => recipeRepository.save(sugarWithEggRecipeWithoutId));
-    for (final ingredient in sugarWithEggRecipeWithoutId.ingredients) {
-      verify(() => recipeIngredientRepository.save(RecipeIngredientEntity(
-            parentRecipeId: savedRecipe.id!,
-            recipeIngredientId: ingredient.id,
-            quantity: ingredient.quantity,
-            type: ingredient.type,
-          )));
-    }
-    verify(() => database.insideTransaction(any()));
+    expect(result.getLeft().toNullable(), failure);
+    verify(() => repository.save(cakeRecipe));
   });
 }
