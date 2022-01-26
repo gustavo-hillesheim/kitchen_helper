a0b79b129c37f688e61e4f502231406e91772e86
gustavo-hillesheim <guhillesheim@gmail.com>
2022-01-26 13:50:41 -0300

    Added deview_preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -5,5 +7,8 @@ import 'app_module.dart';
 import 'presenter/presenter.dart';
 
 void main() {
-  runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+  runApp(DevicePreview(
+    builder: (_) => ModularApp(module: AppModule(), child: const AppWidget()),
+    enabled: !kReleaseMode,
+  ));
 }
