3f59077288440799cd34125e16443e32b06b1e39
gustavo-hillesheim <guhillesheim@gmail.com>
2022-02-25 10:57:36 -0300

    Configured Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:isolate';
 
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -8,9 +12,28 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'app_module.dart';
 import 'app_widget.dart';
 
-void main() {
-  runApp(DevicePreview(
-    builder: (_) => ModularApp(module: AppModule(), child: const AppWidget()),
-    enabled: !kReleaseMode && Platform.isWindows,
-  ));
+void main() async {
+  runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp();
+      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+      // Listens for errors outside of Flutter
+      Isolate.current.addErrorListener(RawReceivePort((pair) async {
+        final List<dynamic> errorAndStacktrace = pair;
+        await FirebaseCrashlytics.instance.recordError(
+          errorAndStacktrace.first,
+          errorAndStacktrace.last,
+        );
+      }).sendPort);
+
+      runApp(DevicePreview(
+        builder: (_) =>
+            ModularApp(module: AppModule(), child: const AppWidget()),
+        enabled: !kReleaseMode && Platform.isWindows,
+      ));
+    },
+    // Listens for errors inside the zone
+    (e, s) => FirebaseCrashlytics.instance.recordError(e, s),
+  );
 }
